fdcfbbf57fae8e7afc1bda65452eef63aec6645e
SyedAbdullawaseem <syedabdullawaseem635@gmail.com>
2022-12-03 13:53:50 +0530

    React

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,22 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Expense Items</div><br></br>
+      <div>
+
+        <div>Food Rs 10</div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+      </div>
+    </div>
+    
+  );
+}
+
+export default ExpenseItem;
+
+// Food Rs 10
+
+// 	Petrol Rs 100
+
+// 	Movies Rs 200
